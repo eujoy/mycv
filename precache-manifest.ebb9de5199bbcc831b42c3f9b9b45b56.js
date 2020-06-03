a1685f8960d1108ec3ccd6d8b72ada915b0839e7
Angelos Giannis <angelos.giannis@gmail.com>
2020-06-03 19:56:19 +0300

    Updates

---
+++ precache-manifest.ebb9de5199bbcc831b42c3f9b9b45b56.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73cd2f191ac93244257927f7f10560a4",
+    "url": "/mycv/index.html"
+  },
+  {
+    "revision": "4fb70f446cb89215a99b",
+    "url": "/mycv/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9ea815a5d3fec3b39ebb",
+    "url": "/mycv/static/js/2.b4e59c80.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mycv/static/js/2.b4e59c80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4fb70f446cb89215a99b",
+    "url": "/mycv/static/js/main.3661b7c3.chunk.js"
+  },
+  {
+    "revision": "11c2afd8d99341db2ed8",
+    "url": "/mycv/static/js/runtime-main.14fd0c85.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mycv/static/media/logo.5d5d9eef.svg"
+  }
+]);
